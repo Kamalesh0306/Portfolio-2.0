f40cf32cdfee2763da6a6cb2227e859a7f808bf2
Kamalesh0306 <kamaleshsand1114@gmail.com>
2024-03-18 20:09:51 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,111 @@
+import Covid from '../assets/projects/Covid.png'
+import Dic from '../assets/projects/Dic.png'
+import Emp from '../assets/projects/Emp.png'
+import Gym from '../assets/projects/Gym.png'
+import Lib from '../assets/projects/Lib.png'
+import Weather from '../assets/projects/Wer.png'
+import Img from '../assets/projects/ai.png'
+import Dog from '../assets/projects/dog.png'
+import News from '../assets/projects/news.png'
+import TODO from '../assets/projects/todo.png'
+import TTS from '../assets/projects/tts.png'
+import Video from '../assets/projects/video.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Dog Website",
+        image:Dog,
+        github:"https://github.com/Kamalesh0306/Dog-Website",
+        live:"https://kamalesh0306.github.io/Dog-Website/",
+    },
+    {
+        id:2,
+        name:"Employee-Detail-Form",
+        image:Emp,
+        github:"https://github.com/Kamalesh0306/Employee-Detail-Form",
+        live:"https://employee-detail-form.vercel.app/",
+    },
+    {
+        id:3,
+        name:"Gym-App",
+        image:Gym,
+        github:"https://github.com/Kamalesh0306/Gym-App",
+        live:"https://gym-app-01.vercel.app/",
+    },
+    {
+        id:4,
+        name:"Covid-App",
+        image:Covid,
+        github:"https://github.com/Kamalesh0306/Covid",
+        live:"https://covid01.vercel.app/",
+    },
+
+    {
+        id:5,
+        name:"Library-Management",
+        image:Lib,
+        github:"https://github.com/Kamalesh0306/Library",
+        live:"https://library-01.vercel.app/",
+    },
+
+    {
+        id:6,
+        name:"AI-Image-Generator",
+        image:Img,
+        github:"https://github.com/Kamalesh0306/AI-Image-Generator",
+        live:"https://ai-image-generator-01.vercel.app/",
+    },
+
+    {
+        id:7,
+        name:"Dictionary-App",
+        image:Dic,
+        github:"https://github.com/Kamalesh0306/Dictionary-App",
+        live:"https://github.com/Kamalesh0306/Dictionary-App",
+    },
+
+    {
+        id:8,
+        name:"TO-DO",
+        image:TODO,
+        github:"https://github.com/Kamalesh0306/TO-DO",
+        live:"https://to-do-kam.vercel.app/",
+    },
+
+    {
+        id:9,
+        name:"News-App",
+        image:News,
+        github:"https://github.com/Kamalesh0306/News-App",
+        live:"https://github.com/Kamalesh0306/News-App",
+    },
+
+    {
+        id:10,
+        name:"Text-To-Speech",
+        image:TTS,
+        github:"https://github.com/Kamalesh0306/Text-To-Speech",
+        live:"https://text-to-speech-01.vercel.app/",
+    },
+
+    {
+        id:11,
+        name:"Video-Chat-App",
+        image:Video,
+        github:"https://github.com/Kamalesh0306/Video-Chat-App",
+        live:"https://github.com/Kamalesh0306/Video-Chat-App",
+    },
+
+    {
+        id:12,
+        name:"Weather-App",
+        image:Weather,
+        github:"https://github.com/Kamalesh0306/Weather-app",
+        live:"https://my-weather-app01.vercel.app/",
+    },
+
+   
+
+]
+
